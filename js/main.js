38657e6bbd1bc4f7dabc24ec55cbb39d6e883efb
DaryaV@munich.quantag-it.com <darya.vyshnyakova@quantag-it.com>
2018-09-18 17:02:42 +0300

    com

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	var text = $('#text1').text(),
+		wordArray = [],
+		splitWords = text.split(' '),
+		counter = 400;
+
+	$(splitWords).each(function(index){
+		var markup = '<span style="position: relative; font-size:60px; right:' + counter + ' px"> ' + splitWords[index] + '</span>'
+	wordArray.push(markup);
+	counter = counter + 200;
+	});
+
+	var finallArray = wordArray. join(' ');
+	$('#text1').html(finallArray);
+
+	$('.wrapper').on('mouseenter', function(){
+	var tl = new TimelineMax();
+		
+		tl.staggerTo($('#text1 span'), 0.5, {right: 0}, 1);
+	});
+});
